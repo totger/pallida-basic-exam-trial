ccaf851cfe179aceae913ec75133747aa6146b34
Gergo Toth <toothgergo@gmail.com>
2017-09-18 11:14:27 +0200

    Dictionary add and remove funtion

---
+++ ThirdTaskDictionary/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static Dictionary<string, string> Dictionary;
+
+        static void Main(string[] args)
+        {
+            Dictionary = new Dictionary<string, string>
+            {
+                { "alma", "apple" },
+                { "fa", "tree" },
+                { "bicikli", "bicycle" },
+                { "asztal", "table"}
+            };
+
+            AddWord("kutya", "dog");
+            RemoveWord("asztal");
+
+            //add more words to your dictionary
+
+        }
+
+        // Implement this method. It should add the given key-value pair to the the dictionary
+        public static void AddWord(string hungarianWord, string englishWord)
+        {
+            Dictionary.Add(hungarianWord, englishWord);
+        }
+
+        // Implement this method. It should remove the key-value pair by the given key from the dictionary
+        public static void RemoveWord(string hungarianWord)
+        {
+            Dictionary.Remove(hungarianWord);
+        }
+
+        // Implement a method which works as a translator from Hungarian to English
+        // Example: you give it a parameter "alma" and it's output is "tree"
+        public static string TranslateToEnglish(string hungarian)
+        {
+            return 
+        }
+
+        // Implement a method which works as a translator from English to Hungarian
+        // Example: you give it a parameter "apple" and it's output is "alma"
+        public static string TranslateToHungarian(string english)
+        {
+
+        }
+    }
+}
