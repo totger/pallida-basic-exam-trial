6fc98add4f589ee6b4b61c9abc3e5ca7f56c6a6c
Gergo Toth <toothgergo@gmail.com>
2017-09-18 10:01:26 +0200

    OddFilter function added

---
+++ FirstTaskOddFilter/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OddFilter
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Create an OddFilter function that takes a list as a parameter,
+			// and returns a new list with every odd element from the orignal list
+			List<int> oddElements = OddFilter(new List<int> { 1, 2, 3, 4, 5 });
+
+			// In case of the example input above, the given PrintList function should print 1 3 5 
+			PrintList(oddElements);
+
+			Console.ReadLine();
+		}
+
+        private static List<int> OddFilter(List<int> list)
+        {
+            List<int> returnList = new List<int>();
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] % 2 == 1)
+                {
+                    returnList.Add(list[i]);
+                }
+            }
+            return returnList;
+        }
+
+        private static void PrintList(List<int> list)
+		{
+			foreach (int element in list)
+			{
+				Console.Write("{0} ", element);
+			}
+		}
+	}
+}
